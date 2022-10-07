39082deffe7ae199a156f92deda44a58ea5f556f
Cjga7 <cristiangarciaalanis6@gmail.com>
2022-10-07 11:18:58 -0400

    "codigo fuente sin herencia"

--- Practica Herencia/src/Ciudad.java
+++ Practica Herencia/src/Ciudad.java
@@ -9,4 +9,28 @@ public class Ciudad {
         this.codCiudad = codCiudad;
         this.nombre = nombre;
     }
+
+    public String getCodCiudad() {
+        return codCiudad;
+    }
+
+    public void setCodCiudad(String codCiudad) {
+        this.codCiudad = codCiudad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Ciudad{" +
+                "codCiudad='" + codCiudad + '\'' +
+                ", nombre='" + nombre + '\'' +
+                '}';
+    }
 }
--- Practica Herencia/src/Hospital.java
+++ Practica Herencia/src/Hospital.java
@@ -20,4 +20,64 @@ public class Hospital {
         this.servicio = servicio;
         this.director = director;
     }
+
+    public String getCodHospital() {
+        return codHospital;
+    }
+
+    public void setCodHospital(String codHospital) {
+        this.codHospital = codHospital;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Ciudad getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(Ciudad ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public List<Servicio> getServicio() {
+        return servicio;
+    }
+
+    public void setServicio(List<Servicio> servicio) {
+        this.servicio = servicio;
+    }
+
+    public Medico getDirector() {
+        return director;
+    }
+
+    public void setDirector(Medico director) {
+        this.director = director;
+    }
+
+    @Override
+    public String toString() {
+        return "Hospital{" +
+                "codHospital='" + codHospital + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", ciudad=" + ciudad +
+                ", telefono=" + telefono +
+                ", servicio=" + servicio +
+                ", director=" + director +
+                '}';
+    }
 }
